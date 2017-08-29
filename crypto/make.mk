ff0964ab945b90b95b8760632946989e177be4aa
Mike Wilson <mike@stablecc.com>
2017-08-28 18:38:08 -0700

    Crypto++ library

---
+++ crypto/make.mk
@@ -0,0 +1,13 @@
+# Works with https://github.com/stablecc/make
+
+BLDLIBS += $(BASE)/import/crypto
+
+CPPFLAGS += -I $(BASE)/import/crypto/crypto-5.6.5
+CXXFLAGS += -DCRYPTOPP_NO_UNALIGNED_DATA_ACCESS
+#-DNO_OS_DEPENDENCE
+
+ifeq ($(BLDTYPE),debug)
+LDFLAGS += -lcryptoccd
+else
+LDFLAGS += -lcryptocc
+endif
---
+++ crypto/unittest/main.cc
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include "cryptlib.h"
+#include "modes.h"
+#include "aes.h"
+#include "misc.h"
+#include "validate.h"
+
+using namespace CryptoPP;
+using namespace std;
+
+void RegisterFactories();
+
+namespace { OFB_Mode<AES>::Encryption s_globalRNG; }
+RandomNumberGenerator & GlobalRNG()
+{
+	return dynamic_cast<RandomNumberGenerator&>(s_globalRNG);
+}
+
+TEST(Cryptocc_test, All) {
+    ASSERT_TRUE(ValidateAll(false));
+}
+
+int main(int argc, char**argv)
+{
+    std::string seed = IntToString(time(NULL));
+    seed.resize(16, ' ');
+
+    OFB_Mode<AES>::Encryption& prng = dynamic_cast<OFB_Mode<AES>::Encryption&>(GlobalRNG());
+	prng.SetKeyWithIV((byte *)seed.data(), 16, (byte *)seed.data());
+
+    RegisterFactories();
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
